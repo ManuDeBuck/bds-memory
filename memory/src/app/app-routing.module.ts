0cb5eaf8e6edd93ea94593fbceee142c7233f24f
Manu De Buck <manu@mdebuck.org>
2022-04-22 10:33:04 +0200

    :sparkles: Add 3 components and routing

--- memory/src/app/app-routing.module.ts
+++ memory/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LearningPageComponent} from "./learning-page/learning-page.component";
+import {MemoryComponent} from "./memory/memory.component";
+import {StartingPageComponent} from "./starting-page/starting-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'learning', component: LearningPageComponent },
+  { path: 'memory', component: MemoryComponent },
+  { path: '**', component: StartingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
